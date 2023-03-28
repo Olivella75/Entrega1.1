06a35179ff8f862a2001f014934f6d575b492e03
Olivella75 <jordiolivella@hotmail.com>
2023-03-28 20:35:46 +0200

    Afegim millores en el codi exercici S1_3_1.js

--- S1_3_1.js
+++ S1_3_1.js
@@ -1,96 +1,20 @@
-let result;
-const matriu = [
-    function one() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += i + ", ";
-        }
-        dades2 = dades.slice(0, dades.length - 2);
-        return dades2;
-    },
-    function two() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-    function three() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-    function four() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-    function five() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-    function six() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-    function seven() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-    function eight() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-    function nine() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-    function ten() {
-        let dades = "";
-        let dades2 = "";
-        for (let i = 0; i <= 9; i++) {
-            dades += `${i} ,`;
-        }
-        dades2 = dades.substring(0, dades.length - 2);
-        return dades2;
-    },
-]
-for (i = 0; i < matriu.length; i++) {
+//Crea una matriu de deu funcions i emplena-la mitjançant un bucle de manera que cada funció compti del 0 al 9 per la consola. 
+//Invoca cada funció de l'array iterativament. Haurà de mostrar-se per consola el compte del 0 al 9 deu vegades.
+
+const matriu = [];
+
+const funcBase = function() {
+    let data = "";
+    for (let i = 0; i < 10; i++) {
+        data += i + " ";
+    }
+    return data;
+}
+
+for (let i = 0; i < 10; i++) {
+    matriu.push(funcBase);
+}
+
+for (let i = 0; i < matriu.length; i++) {
     console.log(matriu[i]());
 }
